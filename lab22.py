5501a657bcabc2377915adeebd1c9553189aef2e
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2026-02-25 09:09:50 -0800

    Update lab22.py

--- lab22.py
+++ lab22.py
@@ -12,12 +12,9 @@ from geometry_msgs.msg import Twist, PoseStamped
 class GotoGoal(Node): # we dont even need this bruh idk what im doing but like itll set a timer to run the go to goal pose once
     def __init__(self):
         super().__init__('go_to_goal_pose')
-
-        self.publisher_goal = self.create_publisher(String, '/goal_pose', 10)
-        self.publisher_iniital = self.create_publisher(String, '/initialpose', 10)
         self.go = True
         if self.go:
-            self.create_timer(1, self.go_to_goal_pose)
+            self.go_to_goal_pose()
             self.go = False
 
     def go_to_goal_pose(self):
@@ -25,34 +22,36 @@ class GotoGoal(Node): # we dont even need this bruh idk what im doing but like i
         init_pose = PoseStamped()
         goal_pose = PoseStamped()
 
+        nav = BasicNavigator()
+
         init_pose.header.frame_id = 'map'
-        init_pose.header.stamp = navigator.get_clock().now().to_msg()
+        init_pose.header.stamp = nav.get_clock().now().to_msg()
         init_pose.pose.position.x = 0.0
         init_pose.pose.position.y = 0.0
         init_pose.pose.orientation.z = 0.0
         init_pose.pose.orientation.w = 1.0
 
         goal_pose.header.frame_id = 'map'
-        goal_pose.header.stamp = navigator.get_clock().now().to_msg()
+        goal_pose.header.stamp = nav.get_clock().now().to_msg()
         goal_pose.pose.position.x = 1.0
         goal_pose.pose.position.y = 1.0
         goal_pose.pose.orientation.z = 0.0
         goal_pose.pose.orientation.w = 1.0
 
 
-        self.setInitialPose(init_pose)
-        self.waitUntilNav2Active() # if autostarted, else use lifecycleStartup()
+        nav.setInitialPose(init_pose)
+        nav.waitUntilNav2Active() # if autostarted, else use lifecycleStartup()
 
-        path = self.getPath(init_pose, goal_pose) # finds path to go to goal pose from inital pose
-        smoothed_path = self.smoothPath(path)
+        path = nav.getPath(init_pose, goal_pose) # finds path to go to goal pose from inital pose
+        #smoothed_path = nav.smoothPath(path)
 
-        self.goToPose(goal_pose) # actually moves to the pose
-        while not self.isTaskComplete():
-            feedback = self.getFeedback()
+        nav.goToPose(goal_pose) # actually moves to the pose
+        while not nav.isTaskComplete():
+            feedback = nav.getFeedback()
         if feedback.navigation_duration > 600:
-            self.cancelTask()
+            nav.cancelTask()
 
-        result = self.getResult()
+        result = nav.getResult()
         if result == TaskResult.SUCCEEDED:
             print('Goal succeeded!')
         elif result == TaskResult.CANCELED:
@@ -64,8 +63,8 @@ class GotoGoal(Node): # we dont even need this bruh idk what im doing but like i
 
 def main(args=None):
     rclpy.init()
-    nav = BasicNavigator()
-    nav.spin()
+    poop = GotoGoal()
+    poop.spin()
 
 
 
