6d28ace79ca20a80cdb2bafa0d4fc9c9af2be4a3
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2026-02-26 21:23:43 -0800

    Update lab24.py

--- lab24.py
+++ lab24.py
@@ -12,6 +12,13 @@ class CameraThing(Node):
         self.bridge = CvBridge()
         self.img_pub = self.create_publisher(Image, '/example_image', 10)
         self.subscription = self.create_subscription(Image, '/oakd/rgb/image_raw', self.callback, 10)
+        self.create_timer(1, self.control_loop)
+        self.publisher_ = self.create_publisher(TwistStamped, '/gobilda/cmd_vel', 10)
+        self.direction = 1
+        self.forward_msg = TwistStamped()
+        self.forward_msg.twist.linear.x = float(0) #1 m/s
+        self.is_red = False
+
 
     def callback(self, msg):
         img = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
@@ -22,8 +29,12 @@ class CameraThing(Node):
 
         mask = cv2.inRange(crop, lower_red, upper_red)
 
-        is_red = np.any(mask > 0)
-        
+        self.is_red = np.any(mask > 0)
+
+   def control_loop(self):
+        if self.is_red: # goes forward if is_red is true
+             self.turn_msg.twist.linear.x = 0.2 * self.direction
+             self.publisher_.publish(self.forward_msg)
         
 
 def main(args=None):
